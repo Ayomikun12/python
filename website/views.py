e11b5cc78f86dad5563f8aea3810111e5ed1c9f9
Ibrahim Ayomikun <olalekanayomikun03@gmail.com>
2026-05-02 14:22:23 +0100

    New

--- website/views.py
+++ website/views.py
@@ -1,17 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for, flash
 from .models import User
 
-from flask import Flask, render_template
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 views = Blueprint('views', __name__)
 
